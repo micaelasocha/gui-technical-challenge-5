f63372dc051fb3a9c8efcff210d23a58c21b3dcd
MICAELA_SOCHA <ms22bm@fsu.edu>
2024-09-22 22:59:49 -0400

    initail comit (followed video), works!

--- sketch.js
+++ sketch.js
@@ -1,7 +1,56 @@
+let d = 60;
+let deg = 58;
+let outline = true;
+
+let gui;
+let diameterSlider, degSlider, oulineCheckbox;
+
+
+
 function setup() {
   createCanvas(400, 400);
+  rectMode(CENTER);
+  strokeWeight(10)
+
+gui = createGui();
+gui.setRounding(1);
+diameterSlider = createSlider("Diameter", 200, 10, 100, 20,100);
+diameterSlider.onChange = diameterSliderChange;
+diameterSlider.setStyle({
+fillBg: color('blue'),
+strokeWeight: 4
+})
+degSlider = createSlider("rotation", 200,40, 100,20, 0,180);
+degSlider.isInteger = true;
+degSlider.val = 23;
+outlineCheckbox = createCheckbox("Outline", 340,10, 20,20, true);
 }
 
 function draw() {
   background(125);
+  drawGui();
+
+  if (outlineCheckbox.val){
+  stroke("black");
+  } else{
+  noStroke();
+  }
+
+
+  circle(100,200,diameterSlider.val);
+
+  push();
+  translate(200,200);
+  rotate(radians(degSlider.val));
+  rect(0,0,60,60);
+  pop();
+
+
+  noStroke();
+  text(diameterSlider.label, 140, 20);
+
+}
+
+function diameterSliderChange() {
+
 }
